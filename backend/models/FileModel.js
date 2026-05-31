d7629e5d156faff3edc529e671392bac472d1ecb
Divya-1610 <divya161006@gmail.com>
2026-05-31 12:19:24 +0530

    deployment commits

--- backend/models/FileModel.js
+++ backend/models/FileModel.js
@@ -1,7 +1,7 @@
 import { Schema,model } from "mongoose";
 
 const fileschema = new Schema({
-    filename:{type:String , minLength:3,required:[true,'file name required'],unique:[true, 'file already exists']},
+    filename:{type:String , minLength:3,required:[true,'file name required']},
     filetype:{type:String , enum: ['pdf', 'png', 'jpg', 'jpeg', 'docx', 'pages', 'txt', 'xlsx', 'zip', 'rar'],lowercase:true, required:[true,'filetype is required']},
     fileUrl: {type: String,required: [true, 'Cloudinary URL is required']},
     size: {type: Number, required: true},
