cfabbc0f86482f6be07aeadd3466672382a3ca0f
Divya-1610 <divya161006@gmail.com>
2026-06-12 08:35:27 +0530

    commit

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -7,7 +7,7 @@ function Header() {
     <div className="text-2xl flex justify-between h-[10vh] p-3 bg-white shadow-2xl">
       <div className='flex gap-2 items-center justify-center'>
         <img src={logo} onClick ={()=> navigate('/')} alt='logo' className='h-full rounded-full hover:cursor-pointer'/>
-        <h1 className='text-2xl font-bold'>Digi<span className='text-blue-700'>Vault</span></h1>
+        <h1 className='text-2xl font-bold'>Cloud<span className='text-blue-700'>Vault</span></h1>
       </div>
       <div className='flex flex-wrap p-3 gap-4'>
         <button onClick={() => navigate('/login')} className='border p-2 px-4 hover:bg-black hover:cursor-pointer text-white bg-blue-700'>Login</button>
